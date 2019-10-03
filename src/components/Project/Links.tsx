25333d6f3f55906a4927707044372f3d604f00c6
alarivan <alarivan@pm.me>
2019-10-03 11:23:55 -0700

    Update project link styles

--- src/components/Project/Links.tsx
+++ src/components/Project/Links.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
 
 interface Props {
   website: string
@@ -10,17 +13,73 @@ const SLinks = styled.ul`
   padding: 0;
   list-style: none;
   display: flex;
-  margin: 0 0 1rem;
+  margin: 0.75rem 0 1rem;
+
+  li {
+    display: block;
+    flex: 1 1 auto;
+
+    &:not(:last-child) {
+      margin-right: 0.5rem;
+    }
+
+    @media (min-width: 768px) {
+      flex: 0 0 auto;
+    }
+  }
 `
 
 const Link = styled.a`
-  color: #444;
+  display: block;
+  color: #111;
   text-decoration: none;
-  font-size: 0.8rem;
-  margin-right: 1rem;
+  font-weight: 500;
+  padding: 0.25rem 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 300ms ease;
+  border: 1px solid;
+
+  @media (min-width: 768px) {
+    padding: 0.5rem 1rem;
+  }
+`
+
+const Website = styled(Link)`
+  background-color: #cbf4cb;
+  border-color: #88bd89;
 
   :hover {
-    text-decoration: underline;
+    background-color: #aceead;
+  }
+`
+const Source = styled(Link)`
+  background-color: #cdccf4;
+  border-color: #9c9bd7;
+
+  :hover {
+    background-color: #adacee;
+  }
+`
+
+const Icon = styled(FontAwesomeIcon)`
+  &.svg-inline--fa.fa-w-16 {
+    width: 0.6rem;
+    margin-right: 0.5rem;
+
+    @media (min-width: 768px) {
+      width: auto;
+    }
+  }
+`
+
+const Title = styled.div`
+  font-size: 0.6rem;
+  text-transform: uppercase;
+
+  @media (min-width: 768px) {
+    font-size: 0.8rem;
   }
 `
 
@@ -28,15 +87,17 @@ const Links: React.FC<Props> = ({ website, source }) => {
   return (
     <SLinks>
       <li>
-        <Link href={website} target="_blank" rel="noopener">
-          website
-        </Link>
+        <Website href={website} target="_blank" rel="noopener">
+          <Icon icon={faExternalLinkAlt} />
+          <Title>view website</Title>
+        </Website>
       </li>
       {source && (
         <li>
-          <Link href={source} target="_blank" rel="noopener">
-            source
-          </Link>
+          <Source href={source} target="_blank" rel="noopener">
+            <Icon icon={faGithub} />
+            <Title>view source</Title>
+          </Source>
         </li>
       )}
     </SLinks>
