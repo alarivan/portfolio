70721e61f26f9c700ecaffa187a13d0fd693b4fa
alarivan <alarivan@pm.me>
2020-12-14 14:11:19 -0800

    Update InfoItem data structure & content

--- src/components/Sidebar/InfoItem.tsx
+++ src/components/Sidebar/InfoItem.tsx
@@ -2,24 +2,34 @@ import React from "react"
 import styled from "styled-components"
 
 const SInfoItem = styled.li`
-  margin-bottom: 0.25rem;
+  margin-bottom: 0.75rem;
   font-size: 0.9rem;
 `
 const Title = styled.div`
   font-weight: bold;
-  margin-bottom: 0.15rem;
+  margin-bottom: 0.35rem;
+`
+
+const DescriptionLine = styled.div`
+  margin-bottom: 0.5rem;
 `
 
 interface Props {
   title: string
-  description: string
+  description: Array<string>
 }
 
 const InfoItem: React.FC<Props> = ({ title, description }) => {
   return (
     <SInfoItem>
       <Title>{title}</Title>
-      <div>{description}</div>
+      <div>
+        {description.map(item => (
+          <DescriptionLine style={{ marginBottom: "0.25rem" }} key={item}>
+            {item}
+          </DescriptionLine>
+        ))}
+      </div>
     </SInfoItem>
   )
 }
